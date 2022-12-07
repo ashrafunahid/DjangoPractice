2f940eb8e8add724775046d53d1fe315fe5b5fdd
Ashraf Uddin <67476419+ashrafunahid@users.noreply.github.com>
2022-12-07 19:14:49 +0600

    ..

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,16 +4,52 @@ from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
+
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.code}"
+    
+    class Meta:
+        verbose_name_plural = "Country Entries"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.street} {self.postal_code} {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f"{self.first_name}, {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name()
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -21,7 +57,6 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
-    
 
     def __str__(self) -> str:
         return f"{self.title} ({self.rating}) {self.author} {self.is_bestselling}"
