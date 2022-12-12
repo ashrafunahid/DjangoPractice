668a361546839467f1a84fa1c53582aeb35ec8df
Ashraf Uddin <67476419+ashrafunahid@users.noreply.github.com>
2022-12-12 19:08:08 +0600

    Session

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.ReviewView.as_view()),
     path("thank-you", views.ThankYouView.as_view()),
     path("review-list", views.ReviewListView.as_view()),
+    path("review-list/favorite", views.AddFavoriteView.as_view()),
     path("review-list/<int:pk>", views.SingleReviewView.as_view()),
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -19,7 +19,7 @@ class ReviewView(CreateView):
     # def form_valid(self, form):
     #     form.save()
     #     return super().form_valid(form)
-    
+
     # def get(self, request):
     #     form = ReviewForm()
 
@@ -91,12 +91,19 @@ class ReviewListView(ListView):
         base_query = super().get_queryset()
         data = base_query.filter(rating__gt=3)
         return data
-    
 
 
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get("favorite_review")
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+        return context
 
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
@@ -104,5 +111,11 @@ class SingleReviewView(DetailView):
     #     selected_review = Review.objects.get(pk=review_id)
     #     context["review"] = selected_review
     #     return context
-    
-    
+
+
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST["review_id"]
+        request.session["favorite_review"] = review_id
+        return HttpResponseRedirect("/review-list/" + review_id)
+         
