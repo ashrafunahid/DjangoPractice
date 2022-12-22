8ecfa697639e8b67eafac7b92b41bb7aff902278
Ashraf Uddin <67476419+ashrafunahid@users.noreply.github.com>
2022-12-22 17:51:34 +0600

    Template Included

--- django_course_site/meetups/views.py
+++ django_course_site/meetups/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Bismillahir Rahmanir Rahim")
+    meetups = [
+        {
+            'title': "First Meetups", 
+            'location': 'New York', 
+            'slug': 'first-meetups'
+        },
+        {
+            'title': "Second Meetups", 
+            'location': 'Paris', 
+            'slug': 'second-meetups'
+        },
+        {
+            'title': "Third Meetups", 
+            'location': 'Dhaka', 
+            'slug': 'third-meetups'
+        },
+    ]
+    return render(request, "meetups/index.html", {
+        'show_meetups': True,
+        'meetups': meetups,
+    })
+
+def meetup_details(request):
+    selected_meetup = {
+        'title': 'A First Meetup',
+        'description': 'This is First Meetup',
+    }
+    return render(request, "meetups/meetup-detail.html", {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description']
+    })
